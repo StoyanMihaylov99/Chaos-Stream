a4e53948a5223c629bc83f09e7a5f8dce183e21e
StoyanMihaylov99 <stoqn.business@gmail.com>
2026-05-19 14:19:42 +0300

    Init the spring boot services: auth, Ingestion, storage, validation,

---
+++ services/storage-service/src/test/java/com/portfolio/chaosstream/storage/StorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.chaosstream.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

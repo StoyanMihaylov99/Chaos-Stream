c34ab19a9c46930e99162878d41e5a6b6500fee8
StoyanMihaylov99 <stoqn.business@gmail.com>
2026-05-20 16:04:32 +0300

    Added gateway service with rate limiting and integrated security configurations. Updated POM and application configurations for gateway and auth services.

---
+++ services/auth-service/src/main/java/com/portfolio/chaosstream/auth/SecurityConfigConstants.java
@@ -0,0 +1,17 @@
+package com.portfolio.chaosstream.auth;
+
+class SecurityConfigConstants {
+
+    static final String LOCALHOST = "http://127.0.0.1:8080" ;
+
+    static final String AUTH_KEYS_PATH = "/auth/v1/keys";
+    static final String AUTH_TOKEN_PATH = "/auth/v1/token";
+    static final String AUTH_AUTHORIZED_PATH = LOCALHOST + "/authorized";
+    static final String AUTH_CLIENT_OIDC_PATH = LOCALHOST + "/login/oauth2/code/messaging-client-oidc";
+
+    static final String CLIENT_ID = "transaction-producer-01";
+    static final String CLIENT_SECRET = "{noop}secret";
+
+    static final String MESSAGE_READ_VALUE = "message.read";
+    static final String MESSAGE_READ_WRITE = "message.write";
+}
